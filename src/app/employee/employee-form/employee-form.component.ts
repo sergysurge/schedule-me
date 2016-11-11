0acfc26fb2647083b1f07f894c5a898179904fcb
Chris Phan <cpp6d@mail.missouri.edu>
2016-11-11 11:28:20 -0800

    MVP for appointment

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import * as moment from 'moment'
 
 import {SelectItem} from 'primeng/primeng';
 
@@ -15,48 +16,82 @@ export class EmployeeFormComponent {
     employees: SelectItem[];
     hours: SelectItem[];
     services: SelectItem[];
-
-    selectedEmployee: string = 'Lucy';
+    start;
+    times;
 
     person = {
-      name: '',
-      number: '',
-      employee: '',
-      date: '',
-      times: '',
-      service: ''
+      contactName: '',
+      contactNumber: '',
+      description: undefined,
+      startTime: undefined,
+      endTime: undefined,
+      customerId :'1',
+      employeeId : '',
+      companyId : '1'
     }
   @Output() clicked = new EventEmitter<string>();
-    onClicked(){
-    alert(this.employees);
-    this.clicked.emit('It works!')
-    }
-    constructor(employeeServiceService:EmployeeServiceService) {
-       
+
+  makeAppointment(employeeServiceService:EmployeeServiceService){
+    let end = moment(this.start)
+    this.person.startTime = end
+    end.add(this.person.description[0].value,'m')
+    this.person.description = this.person.description[0].label
+    this.person.endTime = end
+    console.log('person end',this.person)
+    this.employeeServiceService.makeAppointment(this.person)
+    .then(
+      appointment => {
+        console.log(appointment)
+        }
+    )
+  }
+
+    constructor(private employeeServiceService:EmployeeServiceService) {
+ 
         employeeServiceService.getEmployees(1)
         .subscribe(
           employee => {
           this.employees = employee.json().response.employees
-          console.log("emmmmmmmpppploooyyeees", this.employees)
         }
         )
 
         this.employees = [];
-        this.employees.push({label: 'Lucy', value: 'Audi'});
-        this.employees.push({label: 'Chris', value: 'BMW'});
-        this.employees.push({label: 'Serge', value: 'Fiat'});
-        this.employees.push({label: 'Marco', value: 'Ford'});
         
         this.hours = []
-        this.hours.push({label: "9:00",value: "9:00"})
+        this.hours.push({label: "09:00",value: "09:00"})
+        this.hours.push({label: "09:15",value: "09:15"})
+        this.hours.push({label: "09:30",value: "09:30"})
+        this.hours.push({label: "09:45",value: "09:45"})
         this.hours.push({label: "10:00",value: "10:00"})
+        this.hours.push({label: "10:15",value: "10:15"})
+        this.hours.push({label: "10:30",value: "10:30"})
+        this.hours.push({label: "10:45",value: "10:45"})
         this.hours.push({label: "11:00",value: "11:00"})
+        this.hours.push({label: "11:15",value: "11:15"})
+        this.hours.push({label: "11:30",value: "11:30"})
+        this.hours.push({label: "11:45",value: "11:45"})
         this.hours.push({label: "12:00",value: "12:00"})
+        this.hours.push({label: "12:15",value: "12:15"})
+        this.hours.push({label: "12:30",value: "12:30"})
+        this.hours.push({label: "12:45",value: "12:45"})
+        this.hours.push({label: "13:00",value: "13:00"})
+        this.hours.push({label: "13:15",value: "13:15"})
+        this.hours.push({label: "13:30",value: "13:30"})
+        this.hours.push({label: "13:45",value: "13:45"})
+        this.hours.push({label: "14:00",value: "14:00"})
+        this.hours.push({label: "14:15",value: "14:15"})
+        this.hours.push({label: "14:30",value: "14:30"})
+        this.hours.push({label: "14:45",value: "14:45"})
+        this.hours.push({label: "15:00",value: "15:00"})
+        this.hours.push({label: "15:15",value: "15:15"})
+        this.hours.push({label: "15:30",value: "15:30"})
+        this.hours.push({label: "15:45",value: "15:45"})
+
 
         this.services =[]
-        this.services.push({label: "Manicure",value: "Manicure"})
-        this.services.push({label: "Pedicure",value: "Pedicure"})
-        this.services.push({label: "Wax",value: "Wax"})
-        
+        this.services.push({label: "Manicure",value: 15})
+        this.services.push({label: "Pedicure",value: 30})
+        this.services.push({label: "Wax",value: 15})  
   }
+
 }
--- src/app/employee/employee-service.service.ts
+++ src/app/employee/employee-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -17,10 +18,12 @@ export class EmployeeServiceService {
       //   console.log(response)
       // })
   }
-  makeAppointment(body: Object): Observable<any> {
+  makeAppointment(body: Object): Promise<any> {
     return this.http.post('/api/appointments', body)
-      .map((response: Response) => response)
+      .toPromise()
+      .then(response => response.json())
   }
+
   getEmployees(id: Number): Observable<any> {
     return this.http.get('api/users/employees/?companyId='+ id)
       .map((response: Response) => response)
