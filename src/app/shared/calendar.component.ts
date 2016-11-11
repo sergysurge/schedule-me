e522ce8d483abecbee68ecaf547176a7c87cdebc
Lucy Ji <lucyji1000@gmail.com>
2016-11-10 19:50:39 -0800

    [server-side] added server route to get customer appointments

--- src/app/shared/calendar.component.ts
+++ src/app/shared/calendar.component.ts
@@ -7,26 +7,34 @@ require('style-loader!fullcalendar/dist/fullcalendar.css');
 
 @Component({
   selector: 'app-calendar',
-  template: `
-    <div></div>
-  `
+  template: `<div></div>`
 })
-export class CalendarComponent implements OnInit, AfterViewInit {
+export class CalendarComponent implements AfterViewInit {
 
   constructor(private el: ElementRef) { }
+  @Input() calendarConfig
   schedule: any;
-  x: any = moment("2016-11-10T19:19", moment.ISO_8601);
-  y: any = moment("2016-11-10T22:19", moment.ISO_8601);
+
+  // headers = {
+  //   left: 'prev, next, today',
+  //   center: 'title',
+  //   right: 'month, agendaWeek, agendaDay, listDay'
+  // }
+  // x: any = moment("2016-11-10T19:19", moment.ISO_8601);
+  // y: any = moment("2016-11-10T22:19", moment.ISO_8601);
   
-  events: any[] = [{title: 'blah', start: this.x, end: this.y}];
-  ngOnInit() {
-    console.log(this.x, this.y);
-  }
+  // events: any[] = [{title: 'blah', start: this.x, end: this.y}];
+
+  // options = {
+  //   header: this.headers,
+  //   defaultView: 'agendaWeek',
+  //   events: this.events,
+  //   editable: true
+  // }
+
   ngAfterViewInit() {
     this.schedule = jQuery(this.el.nativeElement.children[0]);
-    this.schedule.fullCalendar({defaultView: 'agendaWeek', events: this.events, editable: true});
-    // this.schedule.fullCalendar({})
-    // console.log(this.schedule.fullCalendar({}))
+    this.schedule.fullCalendar(this.calendarConfig);
   }
 
 }
