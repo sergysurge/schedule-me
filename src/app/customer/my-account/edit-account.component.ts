7779b0db131a61ee8b24876004b8d3b52574f651
Lucy Ji <lucyji1000@gmail.com>
2016-11-15 19:40:09 -0800

    [user details] styling for user edit rout

--- src/app/customer/my-account/edit-account.component.ts
+++ src/app/customer/my-account/edit-account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { CustomerService } from '../customer.service'
+
 @Component({
   selector: 'app-edit-account',
   templateUrl: './edit-account.component.html',
