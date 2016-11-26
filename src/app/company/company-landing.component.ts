02ad12f60b8b753254316faf11bc2a6b16ba4981
sergysurge <sergysarkisyan@gmail.com>
2016-11-26 14:17:20 -0800

    [featureCSS] finished admin dashboard, looks pretty and functions great

--- src/app/company/company-landing.component.ts
+++ src/app/company/company-landing.component.ts
@@ -6,114 +6,7 @@ import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-company-landing',
-  template: `
-    <div class="panel panel-default container-fluid">
-
-      <div class="panel-heading col-sm-12 sergPanelBody">
-          <div class="panel-title">
-              <h1 *ngIf="startedAddCompany" class="text-center"> Create a Company </h1>
-              <h1 *ngIf="!startedAddCompany">Your Companies 
-              <button  *ngIf="step0" class="btn btn-default" type="submit" (click)="step0to1()"> <span class="glyphicon glyphicon-plus-sign" > </span>
-              Add Company </button>  
-              </h1>
-
-          </div>
-      <div>
-
-      <div class="panel-body col-sm-12">
-
-      <div *ngIf="step1" class="col-sm-12">
-        <div class="col-sm-6">
-          <h3> Select a Brand Name </h3>
-            <p *ngIf="selectedBrandNamesAll.id"> Selected : {{selectedBrandNamesAll.id}} {{selectedBrandNamesAll.name}} </p> 
-          <select multiple class="form-control">
-              <option *ngFor="let b of companyService.brandNamesAll" (click)="companySelectedBrandNamesAll(b.id, b.name)"> {{b.id}} {{b.name}} </option>
-          </select>
-          <button class="btn btn-default col-sm-offset-2 col-sm-2" [disabled]="selectedBrandNamesAll.id === false" (click)="companySetBrand()"> select brand </button>
-        </div>
-        <div class="col-sm-6">
-          <h3> Create a Brand Not Listed</h3>
-          <p *ngIf="selectedBrandNamesAll.id"> Selected : {{selectedBrandNamesAll.id}} {{selectedBrandNamesAll.name}} </p> 
-          <input [(ngModel)]="brandNameCustom" placeholder="Your Brand Name Here">
-          <button class="btn btn-default" [disabled]="brandNameCustom === ''" (click)="companyPostBrandName()">CREATE!</button>
-          <h3 *ngIf="brandNameExists === 'no'"> Success! </h3>
-          <p *ngIf="brandNameExists === 'no'"> Please find your brand name in the list and submit! </p>
-          <h3 *ngIf="brandNameExists === 'yes'"> Brand exists! Try Again! </h3>
-        </div>
-      </div> 
-
-      <div *ngIf="step2" class="col-sm-12">
-        <h3> Setting Up Your Profile </h3>
-        <p> We just need some more information </p>
-        
-        <form class="form horizontal" [formGroup]="addCompanyForm" (ngSubmit)="companyUpdateProfile()">
-          <div class="form-group">
-          <label for="BrandNameId" class="col-sm-2 control-label">BrandNameId</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="BrandNameId" [value]="this.companyService.company.BrandNameId" formControlName="BrandNameId">
-          <span class="help-block" id="helpBlock"> enter your BrandNameId here</span>
-        </div>
-        <label for="address" class="col-sm-2 control-label">address</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="address" [(ngModel)]="this.companyService.company.address" formControlName="address">
-          <span class="help-block" id="helpBlock"> enter your address here</span> {{this.companyService.company.address}}
-        </div>
-        <label for="description" class="col-sm-2 control-label">description</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="description" [(ngModel)]="this.companyService.company.description" formControlName="description">
-          <span class="help-block" id="helpBlock"> enter your description here</span>
-        </div>
-        
-        <label for="image" class="col-sm-2 control-label">image</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="image" [(ngModel)]="this.companyService.company.image" formControlName="image">
-          <span class="help-block" id="helpBlock"> enter your image here</span>
-        </div>
-        <label for="logo" class="col-sm-2 control-label">logo</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="logo" [(ngModel)]="this.companyService.company.logo" formControlName="logo">
-          <span class="help-block" id="helpBlock"> enter your logo here</span>
-        </div>
-         <label for="name" class="col-sm-2 control-label">name</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="name" [(ngModel)]="this.companyService.company.name" formControlName="name">
-          <span class="help-block" id="helpBlock"> enter your store name here</span>
-        </div>
-        <label for="phoneNumber" class="col-sm-2 control-label">phoneNumber</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="phoneNumber" [(ngModel)]="this.companyService.company.phoneNumber" formControlName="phoneNumber">
-          <span class="help-block" id="helpBlock"> enter your phoneNumber here</span>
-        </div>
-        <label for="website" class="col-sm-2 control-label">website</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" aria-describedby="helpBlock" id="website" [(ngModel)]="this.companyService.company.website" formControlName="website">
-          <span class="help-block" id="helpBlock"> enter your website here</span>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <button type="submit" class="btn btn-default" > create </button>
-        </div>
-          </div>
-        </form>
-
-        <div
-      </div>
-
-      <div class="col-sm-12" *ngIf="!startedAddCompany">
-
-        <ul>
-          <li *ngFor="let compies of this.companyService.companiesLanding" (click)="navigateToCompany(compies.id)">
-          {{compies.name}}
-          {{compies.address}}
-          </li>
-        </ul>
-      </div>
-
-      </div>
-
-    </div>
-  `,
+  templateUrl: './company-landing.component.html',
   styles: [
     `
     .companyProfilePic {
@@ -137,6 +30,17 @@ import { Router } from '@angular/router'
 .compDesc {
   list-style: none;
   margin: auto;
+}
+
+.adminCompModules:hover {
+  background-color: #008ea8;
+  color: white;
+  margin-bottom : 5px;
+}
+
+.adminCompModules:hover li {
+  background-color: #008ea8;
+  color: white;
 }
 
 .down20 {
